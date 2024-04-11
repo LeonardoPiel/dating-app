02ac50805786e76ff46181cf607cae5010c110ba
Leonardo Piel <leonardo.piel@outlook.com>
2024-04-10 23:28:39 -0300

    adding client #s 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,10 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddDbContext<DataContext>(options =>
 {
 	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -21,6 +24,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+string[] origins = { "http://localhost:4200", "https://localhost:4200" };
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyHeader().WithOrigins(origins));
 
 app.MapControllers();
 
